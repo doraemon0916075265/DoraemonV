398ad431b0a6bd5794408a4ae256da4b2a02e0f4
cathay <cathay@FA17>
2019-03-15 13:14:15 +0800

    wen20190315 test

--- src/cti/app/view/SpecInfoView.java
+++ src/cti/app/view/SpecInfoView.java
@@ -11,8 +11,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 import cti.app.constant.SpecInfoConstant;
 import cti.app.controller.SpecInfoController;
